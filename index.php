02252aebaae3eae81b25ec826b34787fd3bc793f
Maxiii62 <maxime.roussel1@epsi.fr>
2015-10-01 11:55:20 +0200

    serdftyguhjk

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
-<?php include_once 'vue/struct/header.php'; ?>
+<html class="no-js" lang="fr" >
 
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet WebService Audio</title>
+
+    <link rel="stylesheet" href="/WebServiceProjet/asset/css/normalize.css">
+    <link rel="stylesheet" href="/WebServiceProjet/asset/css/foundation.css">
+
+
+    <script src="../asset/js/vendor/modernizr.js"></script>
+
+</head>
+
+
+<body>
     <div class="row">
         <div class="large-12 columns">
             <form>
@@ -36,4 +50,10 @@
         </div>
     </div>
 
-<?php include_once 'vue/struct/footer.php'; ?>
+    <script src="/WebServiceProjet/asset/js/vendor/jquery.js"></script>
+    <script src="/WebServiceProjet/asset/js/foundation.min.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
+</body>
+</html>
