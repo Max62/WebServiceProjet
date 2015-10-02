8399242ae30c24f7eb9859df72964f9491be0db6
Maxiii62 <maxime.roussel1@epsi.fr>
2015-10-02 08:39:20 +0200

    serdftyguhjk

--- asset/js/LoaditemsFromBDDScript.js
+++ asset/js/LoaditemsFromBDDScript.js
@@ -2,11 +2,12 @@
  * Created by MAXIME on 01/10/15.
  */
 $( document ).ready(function() {
-    chargerLesCategories();
+    loadTypes();
+    loadBooks();
 });
 
 
-function chargerLesCategories(){
+function loadTypes(){
 
     $.ajax({
         method: "GET",
@@ -20,6 +21,22 @@ function chargerLesCategories(){
         }
     });
 };
+
+function loadBooks(){
+
+    $.ajax({
+        method: "GET",
+        url : "/WebServiceProjet/controller/GenreController.php",
+        success: function(response) {
+            var obj = jQuery.parseJSON(response);
+
+            for(var i = 0; i < obj.length;i++){
+                $("#mesGenres .dropdown").append("<li><a href='#'>" + obj[i].nametype + "</a></li>");
+            }
+        }
+    });
+};
+
 
 $( "#searchBox" ).onkeyup(function( event ) {
     alert($( "#searchBox" ).val());
