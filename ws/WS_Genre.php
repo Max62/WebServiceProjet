2b9b15b1d04aae6ca877fb4fcc208320cde4a717
Maxiii62 <maxime.roussel1@epsi.fr>
2015-10-02 09:18:16 +0200

    serdftyguhjk

--- ws/WS_Genre.php
+++ ws/WS_Genre.php
@@ -17,33 +17,7 @@ class WS_Genre implements IWebServiciable {
     }
 
     public function doGet() {
-        try
-        {
-            $pdo = getConnexion();
-            $pdo->beginTransaction();
-
-            $sql = "SELECT idtype,nametype FROM type";
-
-            $res = $pdo->query($sql);
-
-            if ($res) {
-                return $res->fetchAll();
-            }
-
-        }
-        catch(Exception $e) //en cas d'erreur
-        {
-            //on annule la transation
-            $pdo->rollback();
-
-            //on affiche un message d'erreur ainsi que les erreurs
-            echo 'Tout ne s est pas bien passé voir les erreurs ci-dessous<br />';
-            echo 'Erreur : '.$e->getMessage().'<br />';
-            echo 'N° : '.$e->getCode();
-
-            //on arrête l'exécution s'il y a du code après
-            exit();
-        }
+        return returnOneArray("SELECT idtype,nametype FROM type");
     }
 
     public function doPost() {
--- ws/bdd.ini.php
+++ ws/bdd.ini.php
@@ -16,4 +16,62 @@ catch(Exception $e)
 }
 }
 
+
+function returnOneLine($var){
+     try
+        {
+            $pdo = getConnexion();
+            $pdo->beginTransaction();
+
+
+            $res = $pdo->query($var);
+
+            if ($res) {
+                $row = $res->fetch(PDO::FETCH_ASSOC);
+                return $row;
+            }
+
+        }
+        catch(Exception $e) //en cas d'erreur
+        {
+            //on annule la transation
+            $pdo->rollback();
+
+            //on affiche un message d'erreur ainsi que les erreurs
+            echo 'Tout ne s est pas bien passé voir les erreurs ci-dessous<br />';
+            echo 'Erreur : '.$e->getMessage().'<br />';
+            echo 'N° : '.$e->getCode();
+
+            //on arrête l'exécution s'il y a du code après
+            exit();
+        }
+}
+
+function returnOneArray($var){
+    try
+        {
+            $pdo = getConnexion();
+            $pdo->beginTransaction();
+
+            $res = $pdo->query($var);
+
+            if ($res) {
+                return $res->fetchAll();
+            }
+
+        }
+        catch(Exception $e) //en cas d'erreur
+        {
+            //on annule la transation
+            $pdo->rollback();
+
+            //on affiche un message d'erreur ainsi que les erreurs
+            echo 'Tout ne s est pas bien passé voir les erreurs ci-dessous<br />';
+            echo 'Erreur : '.$e->getMessage().'<br />';
+            echo 'N° : '.$e->getCode();
+
+            //on arrête l'exécution s'il y a du code après
+            exit();
+        }
+}
 ?>
