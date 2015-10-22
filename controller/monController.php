cb725ebedad1656d960cb3eb8ca88da0c1e3d3d0
Maxiii62 <maxime.roussel1@epsi.fr>
2015-10-22 08:49:17 +0200

    Changement

--- controller/monController.php
+++ controller/monController.php
@@ -2,23 +2,28 @@
 	// Commentaires
 	include_once('helper.php');
 
-	const PATH_WEBSERVICES = 'ws';
+	const PATH_WEBSERVICES = '/ws';
 
 	if(!isset($_GET['ws']))
 		Helper::ThrowAccessDenied();
 
 	// We gets the informations of the desired service.
-	$serviceName = ucfirst(strtolower($_GET['ws']).'WS');
-	$servicePath = PATH_WEBSERVICES.'\\'.$serviceName.'.php';
+	$serviceName = 'WS_'.ucfirst(strtolower($_GET['ws']));
 
+	echo 'ServiceName = '.$serviceName.'<br>';
+	//$serviceName = ucfirst(strtolower($_GET['ws']).'WS');
+	$servicePath = PATH_WEBSERVICES.'/'.$serviceName.'.php';
+	//$servicePath = PATH_WEBSERVICES.'/'.$serviceName.'.php';
+
+	echo '<b>'.$servicePath.'</b><br>';;
 	// If the service doesn't exist, we stop the request.
 	if (!file_exists($servicePath))
 		Helper::ThrowAccessDenied();
 
 
-
 		$method = "do".strtoupper($_SERVER['REQUEST_METHOD']);
 
+		echo 'Ma méthode ='.$method;
 	// We create and execute the service.
 	require_once($servicePath);
 	$service = new $serviceName();
--- ws/IWebServiciable.php
+++ ws/IWebServiciable.php
@@ -1,18 +1,18 @@
 <?php
 
 interface IWebServiciable {
-    
-    function __construct($requestParams);
+
+    function __construct();
 
     public function setParameters();
-    
+
     public function doRequest();
     public function doGet();
     public function doPost();
     public function doPut();
     public function doDelete();
     public function doNeedAuth();
-    
+
 }
 
 ?>
