f418236e7c652374871d9f79223afe9b32b47239
Maxiii62 <maxime.roussel1@epsi.fr>
2015-10-22 08:56:12 +0200

    WS

--- controller/monController.php
+++ controller/monController.php
@@ -2,7 +2,7 @@
 	// Commentaires
 	include_once('helper.php');
 
-	const PATH_WEBSERVICES = '/ws';
+	const PATH_WEBSERVICES = '../ws';
 
 	if(!isset($_GET['ws']))
 		Helper::ThrowAccessDenied();
@@ -10,20 +10,17 @@
 	// We gets the informations of the desired service.
 	$serviceName = 'WS_'.ucfirst(strtolower($_GET['ws']));
 
-	echo 'ServiceName = '.$serviceName.'<br>';
 	//$serviceName = ucfirst(strtolower($_GET['ws']).'WS');
 	$servicePath = PATH_WEBSERVICES.'/'.$serviceName.'.php';
 	//$servicePath = PATH_WEBSERVICES.'/'.$serviceName.'.php';
 
-	echo '<b>'.$servicePath.'</b><br>';;
 	// If the service doesn't exist, we stop the request.
 	if (!file_exists($servicePath))
 		Helper::ThrowAccessDenied();
 
+	$method = "do".ucfirst(strtolower($_SERVER['REQUEST_METHOD']));
 
-		$method = "do".strtoupper($_SERVER['REQUEST_METHOD']);
-
-		echo 'Ma méthode ='.$method;
+	echo 'Ma méthode ='.$method;
 	// We create and execute the service.
 	require_once($servicePath);
 	$service = new $serviceName();
--- ws/WS_Genre.php
+++ ws/WS_Genre.php
@@ -4,9 +4,6 @@ require_once '../ws/IWebServiciable.php';
 require_once '../ws/bdd.ini.php';
 require_once '../ws/WS_Genre.php';
 
-
-
-
 class WS_Genre implements IWebServiciable {
 
     const GET_GENRES = 'getAllGenres';
